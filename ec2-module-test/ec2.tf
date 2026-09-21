643772d2b630ef86518dc81329b9cdb6bb49ec22
unknown <jitheshlakkala2007.com>
2026-09-21 21:52:51 +0530

    aws-ec2-module

---
+++ ec2-module-test/ec2.tf
@@ -0,0 +1,13 @@
+module "ec2"{
+    source = "../terraform-aws-ec2-module"
+    sg_id = "sg-06597f7d289c14461"
+    instance_type = "t3.micro"
+}
+
+output "public_ip"{
+    value=module.ec2.public_ip
+}
+
+output "private_ip"{
+   value= module.ec2.private_ip
+}
